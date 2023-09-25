b3f8ff8544644e0c099ae534168b2cf8aca21cf9
David <dvduardo@gmail.com>
2023-09-25 20:18:17 -0300

    Commit inicial

---
+++ src/test/kotlin/TestRunner.kt
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+
+@RunWith(Cucumber::class)
+@CucumberOptions(
+    features = ["src/test/resources/features"],
+    plugin = ["pretty", "json:target/cucumber-reports/Report.json"],
+    glue = ["step-definition"]
+)
+class TestRunner
